1dfa28bc5006b56cae4302c8d348efe40a8338f3
unknown <ian.brad2@mail.dcu.ie>
2015-10-11 22:33:04 +0100

    created connection to database and stored tables to dataframes

--- MIS40750 - Assignment 1 - Ian Brady (15203975).py
+++ MIS40750 - Assignment 1 - Ian Brady (15203975).py
@@ -3,5 +3,16 @@
 Created on Sun Oct 11 22:17:16 2015
 
 @author: Ian
+MIS40750 - Assignment 1
 """
 
+#import relevant libraries
+import sqlite3
+import pandas as pd
+import math
+
+#create connection to database through the variable conn
+conn = sqlite3.connect("C:\\Users\\Ian\\Documents\\Business Analytics\\Analytics Research & Implementation\\Assignment 1\\renewable.db") 
+#use sql querys to read the tables from the database and store each in a pandas dataframe
+df_locations = pd.read_sql_query("SELECT * FROM location;",conn)
+df_ports = pd.read_sql_query("SELECT * FROM ports;",conn)
