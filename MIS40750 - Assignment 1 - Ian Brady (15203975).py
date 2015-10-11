4cf58a1bd1e86c21be18ea71bedfe53d8c471180
unknown <ian.brad2@mail.dcu.ie>
2015-10-11 22:46:31 +0100

    added route_analysis function and completed testing

--- MIS40750 - Assignment 1 - Ian Brady (15203975).py
+++ MIS40750 - Assignment 1 - Ian Brady (15203975).py
@@ -59,3 +59,51 @@ def profit_per_route(distance, production, cost_per_dist, price_per_piece, limit
         sales_value = production * price_per_piece
     profit = sales_value - cost
     return profit
+    
+#funtion to calculate the shortest and most profitable route
+def route_analysis(locations, cost_per_dist, price_per_piece, limit):
+    min_dist = 9999999
+    max_profit = -99999999
+    port1 = 0
+    location1 = 0
+    port2 = 0
+    location2 = 0
+    #iterrate through each row of locations table
+    for i, row in locations.iterrows():
+        #quantity of ports = number of columns minus the first 3 columns
+        ports_qty = len(locations.columns)-3
+        #assign this locations production to variable
+        loc_production = (locations.loc[[i],'production'% i]).values
+        #for each location iterrate through last 3 columns/ports
+        for j in range(0, ports_qty):
+            #assign distance to port j to variable
+            dist_to_port = (locations.loc[[i],'to port (%d)'% j]).values
+            #call function to check if this distance is shorter than previously assigned
+            if shorter_route(min_dist, dist_to_port):
+                #if it is store the distance, port & location
+                min_dist = dist_to_port
+                port1 = j
+                #I realise that 'i' will be updated more times than is necessary but I felt this was cleaner than checking if it needs to be updated
+                location1 = i
+            #call function to calculate the profit of route
+            route_profit = profit_per_route(dist_to_port, loc_production, cost_per_dist, price_per_piece, limit )
+            if route_profit > max_profit:
+               #only update if value is greater than previosuly assigned
+               max_profit = route_profit
+               port2 = j
+               location2 = i
+    print('Shortest possible route: Location (%d) to Port (%d).Distance = (%f)' % (location1, port1, min_dist))
+    print('Most profitable route: Location (%d) to Port (%d).Profit = € (%f)' % (location2, port2, max_profit))
+    
+    
+route_distances(df_locations, df_ports)
+#run analysis to calculate shortest and most profitable route. Assume limit of 200000
+route_analysis(df_locations, 10, 20, 200000)
+#Recommend to build plant at location 4
+
+#Most profitable route (or least loss making) will only differ in extreme and unrealistic circumstances
+route_analysis(df_locations, 60, -20, 300000)
+
+#Send updated dataframe to excel to communicate to team
+df_locations.to_excel("C:\\Users\\Ian\\Documents\\Business Analytics\\Analytics Research & Implementation\\Assignment 1\\Location To Port Distances.xlsx")
+    
